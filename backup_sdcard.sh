f1a2bab14d334855c07f76390cffb2ef52fd9f49
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2014-05-07 20:58:36 +0200

    basic card backuper ready

--- backup_sdcard.sh
+++ backup_sdcard.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
+CARD_DEVICE="/dev/mmcblk0"
+BOOT_DEVICE=$CARD_DEVICE"p1"
+ROOT_DEVICE=$CARD_DEVICE"p2"
+
 echo
 echo "unmounting..."
-sudo umount /dev/mmcblk0p1
-sudo umount /dev/mmcblk0p2
+sudo umount $BOOT_DEVICE 
+sudo umount $ROOT_DEVICE
 echo
-echo "checking /boot ..."
-sudo fsck.msdos -av /dev/mmcblk0p1
+echo "checking $BOOT_DEVICE ..."
+sudo fsck.msdos -av $BOOT_DEVICE
 echo
-echo "checking / ..."
-sudo fsck.ext4 -vfp /dev/mmcblk0p2
+echo "checking $ROOT_DEVICE ..."
+sudo fsck.ext4 -vfp $ROOT_DEVICE
+
+IMAGE_NAME=`date +"%Y%m%d"`".img.xz"
+echo
+echo "copying $CARD_DEVICE to $IMAGE_NAME..."
+sudo dd if=$CARD_DEVICE bs=4M | xz --best --threads=4 -v > $IMAGE_NAME
 
